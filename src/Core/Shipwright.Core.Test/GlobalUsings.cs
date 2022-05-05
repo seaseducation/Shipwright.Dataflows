65f7ca9f1eb0a3719603a55d602ea685266d5513
Sean Terry <seanterry@outlook.com>
2022-05-05 07:07:23 -0400

    Empty core library

---
+++ src/Core/Shipwright.Core.Test/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Proprietary
+// Copyright (c) TTCO Holding Company, Inc. and Contributors
+// All Rights Reserved.
+
+global using FluentAssertions;
+global using Moq;
+global using Xunit;
