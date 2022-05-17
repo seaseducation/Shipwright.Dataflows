e391fa693110580cb8e32b04210ee5bab2500faa
Sean Terry <seanterry@outlook.com>
2022-05-17 10:33:08 -0400

    Empty agent project

---
+++ src/Shipwright.Agent/Program.cs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Proprietary
+// Copyright (c) TTCO Holding Company, Inc. and Contributors
+// All Rights Reserved.
+
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder( args );
+
+await host.RunConsoleAsync();
