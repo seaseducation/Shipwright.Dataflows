5f907cb0d0089d050c6cc4d6ac071d1fcb489b0a
Sean Terry <seanterry@outlook.com>
2022-05-17 11:43:38 -0400

    Command registration

--- src/Shipwright.Agent/Program.cs
+++ src/Shipwright.Agent/Program.cs
@@ -2,8 +2,21 @@
 // Copyright (c) TTCO Holding Company, Inc. and Contributors
 // All Rights Reserved.
 
+using Lamar.Microsoft.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shipwright.Commands;
 
 var host = Host.CreateDefaultBuilder( args );
+
+host.UseLamar( registry =>
+{
+    registry.Scan( scanner =>
+    {
+        scanner.AssemblyContainingType<ICommandDispatcher>();
+        scanner.AssemblyContainingType<Program>();
+        scanner.WithDefaultConventions();
+        scanner.ConnectImplementationsToTypesClosing( typeof(ICommandHandler<,>) );
+    } );
+} );
 
 await host.RunConsoleAsync();
