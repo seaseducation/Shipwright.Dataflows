a78d7e1300c5f57ddc597fc3f8b6e03f557547d3
Sean Terry <seanterry@outlook.com>
2022-06-24 06:11:32 -0400

    Discovery of typed settings

---
+++ src/Shipwright.Core/Actions/IActionSettings.cs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Proprietary
+// Copyright (c) TTCO Holding Company, Inc. and Contributors
+// All Rights Reserved.
+
+namespace Shipwright.Actions;
+
+/// <summary>
+/// Marker interface for settings that can be created with <see cref="IActionSettingsFactory{TSettings}"/>
+/// </summary>
+public interface IActionSettings {}
--- src/Shipwright.Core/Actions/IActionSettingsFactory.cs
+++ src/Shipwright.Core/Actions/IActionSettingsFactory.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 namespace Shipwright.Actions;
 
 /// <summary>
-/// Factory for obtaining configuration information for actions.
+/// Defines a factory for obtaining configuration information for actions.
 /// </summary>
 public interface IActionSettingsFactory
 {
@@ -25,4 +25,29 @@ public interface IActionSettingsFactory
     /// <typeparam name="TAction">Type of the action whose configuration to build.</typeparam>
     public IConfigurationRoot For<TAction>( ActionContext context ) =>
         For( typeof(TAction).Name, context );
+
+    /// <summary>
+    /// Creates typed configuration settings for an action.
+    /// </summary>
+    /// <param name="context">Context that defines the tenant for which the action will execute.</param>
+    /// <param name="configuration">Configuration for the action.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <typeparam name="TSettings">Type of the settings object to create.</typeparam>
+    public Task<TSettings> Create<TSettings>( ActionContext context, IConfigurationRoot configuration, CancellationToken cancellationToken )
+        where TSettings : IActionSettings;
+}
+
+/// <summary>
+/// Defines a factory for obtaining action-specific settings of a specific type.
+/// </summary>
+/// <typeparam name="TSettings">Type for which action settings are created.</typeparam>
+public interface IActionSettingsFactory<TSettings> where TSettings : IActionSettings
+{
+    /// <summary>
+    /// Creates typed settings for an action.
+    /// </summary>
+    /// <param name="context">Context that defines the tenant for which the action will execute.</param>
+    /// <param name="configuration">Configuration for the action.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    public Task<TSettings> Create( ActionContext context, IConfigurationRoot configuration, CancellationToken cancellationToken );
 }
