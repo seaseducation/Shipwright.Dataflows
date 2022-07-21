3cbf0663ebad0643046cb1c69555483b6faff091
Sean Terry <seanterry@outlook.com>
2022-07-21 09:58:37 -0400

    DbUpsert column types

--- src/Shipwright.Core/Dataflows/Transformations/DbUpsert.cs
+++ src/Shipwright.Core/Dataflows/Transformations/DbUpsert.cs
@@ -26,6 +26,35 @@ public record DbUpsert : Transformation
     public string Table { get; init; } = null!;
 
     /// <summary>
+    /// Column participation type.
+    /// </summary>
+    public enum ColumnType
+    {
+        /// <summary>
+        /// Key columns are used to locate the existing record.
+        /// Their values are inserted for new records, but never updated.
+        /// </summary>
+        Key,
+
+        /// <summary>
+        /// Insert columns are inserted for new records, but never updated.
+        /// Useful for record insert metadata.
+        /// </summary>
+        Insert,
+
+        /// <summary>
+        /// Update columns are inserted for new records, and updated when their values have changed.
+        /// </summary>
+        Update,
+
+        /// <summary>
+        /// Trigger columns are inserted for new records, and only updated when an Update column value has changed.
+        /// Useful for record update metadata.
+        /// </summary>
+        Trigger,
+    }
+    
+    /// <summary>
     /// Validator for the <see cref="DbUpsert"/> transformation.
     /// </summary>
     public class Validator : AbstractValidator<DbUpsert>
